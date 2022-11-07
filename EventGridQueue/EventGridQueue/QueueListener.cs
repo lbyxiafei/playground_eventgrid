f2721fc92bfd78145d1bf0e02e52d1e12bbf3c02
Binyan Li <binyanli@microsoft.com>
2022-11-06 19:13:49 -0800

    To base 64.

--- EventGridQueue/EventGridQueue/QueueListener.cs
+++ EventGridQueue/EventGridQueue/QueueListener.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks; // Namespace for Task
 using Azure.Identity;
 using Azure.Storage.Queues; // Namespace for Queue storage types
 using Azure.Storage.Queues.Models; // Namespace for PeekedMessage
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Azure;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace EventGridQueue
 {
@@ -52,8 +56,25 @@ namespace EventGridQueue
 
                 foreach (QueueMessage message in receivedMessages)
                 {
+                    //var data = Convert.ToBase64String(message.Body.ToArray());
+                    var buffer = Convert.FromBase64String(message.Body.ToString());
+                    Console.WriteLine(buffer);
+                    var data = Encoding.UTF8.GetString(buffer);
+                    Console.WriteLine(data);
+
+                    //var buffer = message.Body.ToArray();
+
+                    //string returnValue;
+
+                    //using (MemoryStream ms = new MemoryStream(buffer))
+                    //{
+                    //    ms.Position = 0;
+                    //    BinaryFormatter bf = new BinaryFormatter();
+                    //    returnValue = (string)bf.Deserialize(ms);
+                    //}
+
                     // Process (i.e. print) the messages in less than 5 minutes
-                    Console.WriteLine($"De-queued message: '{message.Body}'");
+                    Console.WriteLine($"De-queued message: '{data}'");
 
                     // Delete the message
                     await _queueClient.DeleteMessageAsync(message.MessageId, message.PopReceipt).ConfigureAwait(false);
